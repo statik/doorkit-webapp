31273b20a8b9d502017473fdbc321481c3620d4a
Abe Fettig <abe@fettig.net>
2013-05-08 10:11:17 -0400

    Server app.html rendered with Hogan (flavor of Mustache) template

--- webserver.js
+++ webserver.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+app.set('view engine', 'html')
+app.engine('html', require('hogan-express'))
 
 app.get('/', function(request, response) {
-  response.send('Hello ' + process.env.AUDIENCE || 'World');
+  response.render('app', {
+    DOORKIT_URL: process.env.DOORKIT_URL,
+  });
 });
 
 var port = process.env.PORT || 5000;
